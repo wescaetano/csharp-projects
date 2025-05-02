6a1bb12bc300b438e8dec07ba83f45d200496dcf
wescaetano <weslleycaetanosoares@gmail.com>
2025-05-02 02:10:00 -0300

    feat: add Calculator, Strings and TextEdItor

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var id = Guid.NewGuid();
+            //id.ToString();
+            //id = new Guid("df615040-99bc-4d84-b647-690c5adf8579");
+            //Console.WriteLine(id.ToString().Substring(0,8));
+
+            var texto = " este texto é um teste ";
+            //Console.WriteLine(texto.Contains("Teste"));
+            //Console.WriteLine(texto.Contains("teste"));
+            //Console.WriteLine(texto.Contains("Teste", StringComparison.OrdinalIgnoreCase));
+
+            //Console.WriteLine(texto.StartsWith("Este"));
+            //Console.WriteLine(texto.StartsWith("este"));
+            //Console.WriteLine(texto.EndsWith("Teste"));
+            //Console.WriteLine(texto.EndsWith("teste"));
+
+            //Console.WriteLine(texto.IndexOf("é"));
+            //Console.WriteLine(texto.LastIndexOf("s"));
+
+            //Console.WriteLine(texto.ToLower());
+            //Console.WriteLine(texto.ToUpper());
+            //Console.WriteLine(texto.Insert(5, "aqui "));
+            //Console.WriteLine(texto.Remove(5, 5));
+            //Console.WriteLine(texto.Length);
+            //Console.WriteLine(texto.Replace("este", "isto"));
+
+            //var divisao = texto.Split(" ");
+            //Console.WriteLine(divisao[0]);
+            //Console.WriteLine(divisao[1]);
+            //Console.WriteLine(divisao[2]);
+            //Console.WriteLine(divisao[3]);
+            //Console.WriteLine(divisao[4]);
+
+            //var resultado = texto.Substring(5, 5);
+            //Console.WriteLine(resultado);
+
+            Console.WriteLine(texto.Trim());
+
+            var texto2 = new StringBuilder();
+
+            texto2.Append("primeiro texto");
+            texto2.Append(" segundo texto");
+
+            Console.WriteLine(texto2);
+        }
+    }
+}
---
+++ TextEditor/TextEditor/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace TextEditor
+{
+    class Program
+    {
+
+    static void Main(string[] args)
+        {
+            Menu();
+        }
+    
+    static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("O que você deseja fazer?");
+            Console.WriteLine("1- Abrir arquivo");
+            Console.WriteLine("2- Criar novo arquivo");
+            Console.WriteLine("0- Sair");
+
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0:
+                    System.Environment.Exit(0);
+                    break;
+                case 1:
+                    Open();
+                    break;
+                case 2:
+                    Edit();
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida!");
+                    break;
+            }
+        }
+
+    static void Open()
+        {
+            Console.Clear();
+            Console.WriteLine("Qual o caminho do arquivo? ");
+            string path = Console.ReadLine();
+            using(var file = new StreamReader(path))
+            {
+                string text = file.ReadToEnd();
+                Console.WriteLine(text);
+            }
+        }
+    static void Edit()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite seu texto abaixo: ('Esc' para sair) ");
+            Console.WriteLine("--------------------------");
+            string text = "";
+
+            do
+            {
+               text += Console.ReadLine();
+               text += Environment.NewLine;
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Save(text);
+        }
+    
+    static void Save(string text)
+        {
+            Console.Clear();
+            Console.WriteLine("Qual o caminho para salvar o arquivo? ");
+            var path = Console.ReadLine();
+
+            using (var file = new StreamWriter(path))
+            {
+                file.Write(text);
+            }
+
+            Console.WriteLine($"Arquivo {path}salvo com sucesso!");
+            Console.ReadLine();
+            Menu();
+        }
+    }
+}
